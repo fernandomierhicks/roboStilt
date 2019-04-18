3f1a728c6d84be65f1f1cc6e63f712c306623bc9
fernandomierhcks <fernandomierhicks@example.com>
2019-04-18 10:53:45 -0700

    Xacro colors and model in gazebo

---
+++ build/gazebo_ros_demos/rrbot_description/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/fernandomierhicks/roboStilt/src/gazebo_ros_demos/rrbot_description
+# Build directory: /home/fernandomierhicks/roboStilt/build/gazebo_ros_demos/rrbot_description
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
