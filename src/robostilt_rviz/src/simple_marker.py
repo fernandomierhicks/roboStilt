a66343ad013d1d80949b567384a31a029a981f78
fernandomierhcks <fernandomierhicks@example.com>
2019-04-30 08:30:44 -0700

    Marker chaging setpoint not working

--- src/robostilt_rviz/src/simple_marker.py
+++ src/robostilt_rviz/src/simple_marker.py
@@ -6,8 +6,9 @@ from visualization_msgs.msg import *
 from std_msgs.msg import Float64
 
 from geometry_msgs.msg import Pose
+from geometry_msgs.msg import Point
 
-
+from control_msgs.msg import JointTrajectoryControllerState
 from control_msgs.msg import FollowJointTrajectoryGoal
 from control_msgs.msg import FollowJointTrajectoryAction 
 from trajectory_msgs.msg import (
@@ -21,28 +22,64 @@ class myInteractivemarker:
     def __init__(self):
         rospy.init_node("markers")
         self.publisher=rospy.Publisher("/robostilt/trajectory_controller/command", JointTrajectory, queue_size=10)
+        self.listener=rospy.Subscriber("/robostilt/trajectory_controller/state/", JointTrajectoryControllerState, self.joint_position_callback)  
+        self.joint_z=0
         self.server = InteractiveMarkerServer("markers") 
         # create an interactive marker for our server
         int_marker = InteractiveMarker()
-        int_marker.header.frame_id = "leg_2" 
+        int_marker.header.frame_id = "base_link_to_leg_2" 
         int_marker.name = "leg_2_marker"
         int_marker.description = "leg_2"
 
-        # create a grey box marker
-        cyl_marker = Marker()
-        cyl_marker.type = Marker.CYLINDER
-        cyl_marker.scale.x = 0.03
-        cyl_marker.scale.y = 0.03
-        cyl_marker.scale.z = 0.2
-        cyl_marker.color.r = 0.0
-        cyl_marker.color.g = 0.0
-        cyl_marker.color.b = 1.0
-        cyl_marker.color.a = 1.0        
-        # create a non-interactive control which contains the box
-        box_control = InteractiveMarkerControl()
-        box_control.always_visible = True
-        box_control.markers.append( cyl_marker )
-        int_marker.controls.append(box_control)      
+        # create two arrow markers
+        arrow_up = Marker()
+        arrow_up.type = Marker.ARROW
+
+        start_point=Point()
+        start_point.x=0.0
+        start_point.y=0.0
+        start_point.z=1.4   #top of the leg
+
+        end_point=Point()
+        end_point.x=0.0
+        end_point.y=0.0
+        end_point.z=1.4+0.2
+        
+        arrow_up.points.append(start_point)
+        arrow_up.points.append(end_point)
+        arrow_up.scale.x = 0.05        #shaft diameter
+        arrow_up.scale.y = 0.1       #head diameter
+        arrow_up.scale.z = 0.1        #head lenght
+
+        arrow_up.color.r = 0.0
+        arrow_up.color.g = 0.0
+        arrow_up.color.b = 1.0
+        arrow_up.color.a = 1.0      
+
+        #Arrow down
+        arrow_down = Marker()
+        arrow_down.type = Marker.ARROW
+
+        start_point=Point()
+        start_point.x=0.0
+        start_point.y=0.0
+        start_point.z=1.4   #top of the leg
+
+        end_point=Point()
+        end_point.x=0.0
+        end_point.y=0.0
+        end_point.z=1.4-0.2
+        
+        arrow_down.points.append(start_point)
+        arrow_down.points.append(end_point)
+        arrow_down.scale.x = 0.05        #shaft diameter
+        arrow_down.scale.y = 0.1       #head diameter
+        arrow_down.scale.z = 0.1        #head lenght
+
+        arrow_down.color.r = 0.0
+        arrow_down.color.g = 0.0
+        arrow_down.color.b = 1.0
+        arrow_down.color.a = 1.0     
 
 
 
@@ -54,14 +91,14 @@ class myInteractivemarker:
         control.orientation.w = 1
         control.orientation.x = 0
         control.orientation.y = 1
-        control.orientation.z = 0        
+        control.orientation.z = 0    
+        control.always_visible = True    
         control.interaction_mode = InteractiveMarkerControl.MOVE_AXIS
+        control.markers.append(arrow_up)
+        control.markers.append(arrow_down)
         # add the control to the interactive marker      
         
         int_marker.controls.append(control)
-
-        int_marker.scale=0.4
-
 
 
         # add the interactive marker to our collection &
@@ -72,18 +109,20 @@ class myInteractivemarker:
          
 
     def process_feedback(self, feedback):
-
         p = feedback.pose.position
-        print feedback.marker_name + " is now at " + str(p.x) + ", " + str(p.y) + ", " + str(p.z)            
-        #publish to topic
-        trajectory=self.create_trajectory(feedback.pose.position.z,"base_link_to_leg_2")
+        
+        increment=0.01
+        if(p.z < 0.0):
+            increment=increment*-1
 
+
+        print feedback.marker_name + " is now at z=, " + str(p.z) + " joint is at z= " + str(self.joint_z + " publishing "+ str(self.joint_z+increment))           
+        #publish to topic with a small increment in position         
+        trajectory=self.create_trajectory(self.joint_z+increment,"base_link_to_leg_2")
         self.publisher.publish(trajectory)
 
-        #Make marker return to its original position.
-        if feedback.pose.position.z != 0:
-            feedback.pose.position.z = 1.0    #leg lenght /2.0
-            feedback.pose.position.x = 0.2   
+        #permanently position marker the current joint position
+        feedback.pose.position.z = 0
         # Apply changes to the marker.
         self.server.setPose(feedback.marker_name, feedback.pose)
         self.server.applyChanges()   
@@ -98,6 +137,13 @@ class myInteractivemarker:
         trajectory.points[0].time_from_start=rospy.Duration(0.001)
         trajectory.joint_names.append(joint_name)
         return trajectory  
+
+    def joint_position_callback(self,data):
+        #update vartiable with joint posiiont of leg_2
+        self.joint_z = data.actual.positions[2]
+        
+                   
+        
         
 
 if __name__=="__main__":    
