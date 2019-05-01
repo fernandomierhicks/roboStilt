b0affaafd58840688650dedf9715fcd3d24ae12c
fernandomierhicks <fernandomierhicks@gmail.com>
2019-04-30 20:15:55 -0700

    URDF with arguments working

--- src/robostilt_description/src/init_robot.py
+++ src/robostilt_description/src/init_robot.py
@@ -6,6 +6,12 @@ import rospy
 uuid = roslaunch.rlutil.get_or_generate_uuid(None, False)
 roslaunch.configure_logging(uuid)
 
+# # THIS ONLY WORK ON LUNAR:
+# Load Master YAML into parameter server
+# Read individual parameters from Python
+# Launch URDF.xacro launch file with argument from python. 
+# ----Cant do it in Kinetic as roslaunch only acepts arguments in Lunar. ----Cant call xacro from python.
+
 cli_args = ['/home/fernandomierhicks/robostilt/src/robostilt_description/launch/urdf2.launch','leg_lenght:=10.0']
 roslaunch_args = cli_args[1:]
 roslaunch_file = [(roslaunch.rlutil.resolve_launch_arguments(cli_args)[0], roslaunch_args)]
